2197eaa451415d110c156bbabe71466b9b94810c
Saqib <saqib29111@gmail.com>
2020-07-23 00:13:28 +0600

    dart in vs code

---
+++ Dart/dartSources/bin/main.dart
@@ -0,0 +1,13 @@
+void print_code() {
+  print('Hello Dart');
+}
+
+// comments goes like that
+/*
+  multiline comments
+*/
+
+void main() {
+  print('Hello Dart you are like C++');
+  print_code();
+}
