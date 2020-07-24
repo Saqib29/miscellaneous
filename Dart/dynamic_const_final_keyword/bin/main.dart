b46a0629ba7c5b235e42b381d974988b78709858
Saqib <saqib29111@gmail.com>
2020-07-25 00:25:53 +0600

    datatype, variable, dynamic_const_final_keyword, for_loop

---
+++ Dart/dynamic_const_final_keyword/bin/main.dart
@@ -0,0 +1,10 @@
+void main() {
+  dynamic name = "Saqib Aminul";
+
+  const age = 23;
+  final identity = "Bangladesh";
+
+  print("Name: " + name);
+  print("Age: ${age}");
+  print("Identity: " + identity);
+}
---
+++ Dart/variables/bin/main.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  var first_name = "Saqib";   // var is not type safety where String is type safety
+  String last_name = "Aminul";
+
+  print(first_name +" "+last_name);
+
+
+  int a = 5, b = 6;
+  print(a+b);
+  print(a-b);
+  print(a*b);
+  print(a/b);
+}
