b897f105dd2a32cc22149ab820a82034435fddb7
Saqib <saqib29111@gmail.com>
2021-01-16 02:05:30 +0600

    matplotlib

---
+++ python_program/matplotlib_python/matplot.py
@@ -0,0 +1,11 @@
+import numpy as np
+##x = np.linspace(10, 50, 50)
+##y = np.cos(x)
+##z = np.sin(x)
+
+# print(x)
+
+data1 = 2 * np.random.random((10, 10))
+data2 = 3 * np.random.random((10, 10))
+
+print(data1)
