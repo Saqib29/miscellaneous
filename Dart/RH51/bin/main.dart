276b994566c11c200ec101809b81dbf897ea3f09
Saqib <saqib29111@gmail.com>
2021-03-17 21:47:31 +0600

    Data Type + concatenate with strings

---
+++ Dart/RH51/bin/main.dart
@@ -0,0 +1,16 @@
+void main() {
+
+  String fname = "Aminul Islam";
+  var lname = "Saqib";
+
+  int a=100;
+  int b=300;
+  int c=200;
+
+  print(a+b+c);
+  print(a-b+c);
+  print((a*b)+c);
+  print((a+b)/c);
+
+  print(fname+ " " +lname);
+}
