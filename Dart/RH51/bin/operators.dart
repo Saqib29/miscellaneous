db2151361f7cdc0ec28ab7d2949e9c261a38d592
Saqib <saqib29111@gmail.com>
2021-03-17 22:35:26 +0600

    dart operators

---
+++ Dart/RH51/bin/operators.dart
@@ -0,0 +1,14 @@
+void main(){
+  // var arr = new List(5);
+
+  // arr[0] = 1;
+  // print(arr[0]);
+
+  var arr = [1,2,3,4,5,6,7,8,9,10];
+
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i]%2 == 0) {
+      print(arr[i]);
+    }
+  }
+}
