29f1c3fd3762a1f14c4ce3d3cf66b71311e20cea
Saqib <saqib29111@gmail.com>
2021-03-17 23:47:14 +0600

    for, while, do..while loops

--- Dart/RH51/bin/basic.dart
+++ Dart/RH51/bin/basic.dart
@@ -1,8 +1,27 @@
 void main(){
+  print("for loop");
   for (var i = 0; i < 100; i++) {
     if(i%10 == 0){
       print("${i} - value");
       continue;
     }
   }
+
+  print("while loop");
+  int i=0;
+  while (i<100) {
+    if(i%10 == 0){
+      print("${i}");
+    }
+    i++;
+  }
+
+  print("do..while loop");
+  int j = 0;
+  do {
+    if(i%10 == 0){
+      print("${j}");
+    }
+    j++;
+  } while (j<99);
 }
